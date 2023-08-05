bd3aa033a911f735ffe79c37660f9be2ed5cbf2d
arunkkumar2207j <arunkkumar2207j@gmail.com>
2023-08-06 00:36:07 +0530

    Added Cart Component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import CartPage from './pages/CartPage';
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignupPage />,
+  },
+  {
+    path: "/cart",
+    element: <CartPage />,
   },
 ]);
 
--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, ShoppingCartIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Link } from 'react-router-dom'
 
 const user = {
   name: 'Tom Cook',
@@ -61,6 +62,7 @@ export default function NavBar({children}) {
                   </div>
                   <div className="hidden md:block">
                     <div className="ml-4 flex items-center md:ml-6">
+                      <Link to="/cart">
                       <button
                         type="button"
                         className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -68,6 +70,7 @@ export default function NavBar({children}) {
                         <span className="absolute -inset-1.5" />
                         <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
                       </button>
+                      </Link>
                         <span className="inline-flex items-center rounded-md bg-gray-50 mb-5 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
                           3
                         </span>
@@ -152,6 +155,7 @@ export default function NavBar({children}) {
                       <div className="text-base font-medium leading-none text-white">{user.name}</div>
                       <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
                     </div>
+                    <Link to="/cart">
                     <button
                       type="button"
                       className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -162,6 +166,7 @@ export default function NavBar({children}) {
                         Badge
                       </span>
                     </button>
+                    </Link>
                   </div>
                   <div className="mt-3 space-y-1 px-2">
                     {userNavigation.map((item) => (
